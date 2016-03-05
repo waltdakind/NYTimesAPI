34debe7447ebe95e1a10d2d882ec626d7cb39297
Walter Mattingly <waltdakind@gmail.com>
2016-03-05 13:53:46 -0500

    times up

--- backend.js
+++ backend.js
@@ -41,10 +41,27 @@ var Nytsearchurl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" 
 
 //http://api.nytimes.com/svc/search/v2/articlesearch.json?q=Zika&begin_date=20100501&end_date=20160301&api-key=f00d8dbd623a99f7d310810bf38cff90%3A9%3A74629258
 
+	var getStartYear = $('#startyear').val();
+	var getEndYear = $('#endyear').val();
+	var getRecords = $('#records').val();
+	var limit = getRecords; 
+	var searchTerm =  $('#searchTerm').val();
+	var getStartYear = $('#startyear').val();
+	var getEndYear = $('#endyear').val();
 
-var limit = 10; 
-var searchTerm = "";
-var begin_date = "";
+if(getStartYear == "") {
+	var begin_date = "";
+}
+else{
+	begin_date = $('#startyear').val();
+}
+
+if(getEndYear == "") {
+	var end_date = "";
+}
+else{
+	end_date = $('#startyear').val();
+}
 var begin_date_string = "&begin_date=" + begin_date;
 var end_date = "";
 var end_date_string = "&end_date=" + end_date;
