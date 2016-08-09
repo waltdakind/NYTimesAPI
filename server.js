a3b332c5e0614113648b6afff21a86765d1caa13
Walter Mattingly <waltdakind@gmail.com>
2016-08-09 15:24:58 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+    // set up ========================
+    var express  = require('express');
+    var app      = express();
+    var morgan = require('morgan');
+    var bodyParser = require('body-parser');
+    var methodOverride = require('method-override');
+
+    var PORT = process.env.PORT || 8080;
+    // configuration =================
+
+    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+    app.use(morgan('dev'));                                         // log every request to the console
+    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+    app.use(bodyParser.json());                                     // parse application/json
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(methodOverride());
+
